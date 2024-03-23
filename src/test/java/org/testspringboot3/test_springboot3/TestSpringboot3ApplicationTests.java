bf4908cf2eacf1d5148b7411469d47a024a2ef54
abarhub <abarhub@gmail.com>
2024-03-23 19:37:17 +0100

    commit initial

---
+++ src/test/java/org/testspringboot3/test_springboot3/TestSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.testspringboot3.test_springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
